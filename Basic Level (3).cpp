a93552c94a4180797822c1edb67ae2a757601e5d
vacalatyvika02@gmail.com <vacalatyvika02@gmail.com>
2020-03-26 19:16:23 +0300

    Добавьте файлы проекта.

---
+++ Basic Level (3).cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Введите массив символов из 12 елементов.
+// Замените каждый символ-цифру на символ "!".
+int main()
+{
+    cout << "Basic level!" << endl;
+    char sumbol[12];
+    cout << "Enter 12 sumbols: " << endl;
+    for (int i = 0; i < 12; i++)
+    {
+        cin >> sumbol[i];
+    }
+    for (int i = 0; i < 12; i++)
+    {
+        if (isdigit(sumbol[i]))
+            sumbol[i] = '!';
+        cout << sumbol[i];
+    }
+    cout << endl;
+    system("pause");
+}
